1ddf95e2b01d3c7448967d5f466cb30db20480ad
fukky0113 <96651638+fukky0113@users.noreply.github.com>
2024-10-06 01:09:04 +0000

    Add account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated == true
   end
 
   def new
@@ -14,10 +15,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      reset_session
-      log_in @user
-      flash[:success] = 'Welcome to the Sample App!'
-      redirect_to @user
+      @user.send_activation_email
+      flash[:success] = 'Please check your email to activate your account'
+      redirect_to root_url
     else
       render 'new', status: :unprocessable_entity
     end
@@ -35,7 +35,8 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.paginate(page: params[:page])
+    # @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def destroy
